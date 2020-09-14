3dec77bf19487ba5186575b34f3edf90eb970ad7
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-09-14 06:46:47 +0000

    added more methods for game.rb and player.rb

--- game.rb
+++ game.rb
@@ -3,12 +3,22 @@ require_relative "./player"
 class Game
 
   def initialize
-    @round = 1
-  end
-
-  def next_round
-    puts "------new turn--------"
+    @player1 = Player.new("Jeffrey")
+    @player2 = Player.new("Angela")
+    @players=[@player1, @player2]
     
   end
 
+  def random_number
+    rand(20)
+  end
+
+
+  def next_round
+    puts "------new turn--------"
+  end
+
+  def play
+    puts "what does #{random_number} plus #{random_number} equal"
+  end
 end
--- player.rb
+++ player.rb
@@ -1,2 +1,12 @@
 class Player
+
+  def initialize(name)
+    @name =name
+    @lives =3
+  end
+
+  def dead?
+    @lives <=0
+  end
+  
 end
